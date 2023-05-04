901574b6890892c44224c53289a9418b6ddb9636
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-05-04 22:34:00 +0530

    N = int(input())
    for i in range(1, N+1):
        spaces = " "*(N-i)
        num = str(i)*((2*i)-1)
        print(spaces+num)

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,4 +1,6 @@
-n = int(input()) 
+N = int(input())
 
-for i in range(ord('A'), ord('A')+n):
-    print((chr(i)+' ')*n)
+for i in range(1, N+1):
+    spaces = " "*(N-i)
+    num = str(i)*((2*i)-1)
+    print(spaces+num)
