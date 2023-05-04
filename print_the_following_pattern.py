1daebe1f07cbe855ea6ee3011f75ed6b7426f388
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-05-04 22:27:28 +0530

    n = int(input())
    for i in range(ord('A'), ord('A')+n):
        print((chr(i)+' ')*n)

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,8 +1,4 @@
-n = int(input())  # Get user input for number of rows
+n = int(input()) 
 
-# Iterate over numbers from 1 to n
-for i in range(1, n+1):
-    # Iterate over numbers from 1 to i, and print each number
-    for j in range(1, i+1):
-        print(j, end="")
-    print()  # Move to next line after printing each row
+for i in range(ord('A'), ord('A')+n):
+    print((chr(i)+' ')*n)
