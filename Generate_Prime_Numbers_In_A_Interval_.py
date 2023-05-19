6ca223f27618564b0813eb41e9df7503ea51d1d4
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-05-19 18:20:57 +0530

    start = int(input()) end = int(input()) def is_prime(num):     if num <= 1:         return False     for i in range(2, int(num ** 0.5) + 1):         if num % i == 0:             return False     return True for num in range(start, end+1):     if is_prime(num):         print(num)

--- Generate_Prime_Numbers_In_A_Interval_.py
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -1,14 +1,12 @@
 start = int(input())
 end = int(input())
-for num in range(start, end+1):
-    is_prime = True
+def is_prime(num):
     if num <= 1:
-        is_prime = False
-    else:
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                is_prime = False
-                break
-
-    if is_prime:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+for num in range(start, end+1):
+    if is_prime(num):
         print(num)
