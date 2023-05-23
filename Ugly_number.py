02474c7d31c87603e21ba5e84854e9919b23d15d
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-05-23 14:03:08 +0530

    def isUgly(num):     if num <= 0:         return False     if num == 1:         return True     while num % 2 == 0:         num = num / 2     while num % 3 == 0:         num = num / 3     while num % 5 == 0:         num = num / 5     return num == 1 n = int(input()) if isUgly(n):     print("Ugly Number") else:     print("Not Ugly Number")

--- Ugly_number.py
+++ Ugly_number.py
@@ -4,14 +4,14 @@ def isUgly(num):
     if num == 1:
         return True
     while num % 2 == 0:
-        num //= 2
+        num = num / 2
     while num % 3 == 0:
-        num //= 3
+        num = num / 3
     while num % 5 == 0:
-        num //= 5
+        num = num / 5
     return num == 1
-num = int(input())
-if isUgly(num):
+n = int(input())
+if isUgly(n):
     print("Ugly Number")
 else:
     print("Not Ugly Number")
