becc241bb4683db215b5201d6e840c6d67254776
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-08-01 22:10:23 +0530

    def checkSpy(num): 	sums = 0 	product = 1 	while num>0: 		digit = num % 10 		sums = sums + digit 		product = product * digit 		num = num // 10 	if sums == product: 		return True 	else: 		return False num = int(input()) if (checkSpy(num)): 	print("Spy Number") else: 	print("Not Spy Number")

---
+++ Spy_Number.py
@@ -0,0 +1,17 @@
+def checkSpy(num):
+	sums = 0
+	product = 1
+	while num>0:
+		digit = num % 10
+		sums = sums + digit
+		product = product * digit
+		num = num // 10
+	if sums == product:
+		return True
+	else:
+		return False
+num = int(input())
+if (checkSpy(num)):
+	print("Spy Number")
+else:
+	print("Not Spy Number")
