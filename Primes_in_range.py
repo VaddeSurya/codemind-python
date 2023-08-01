c0160aff15da9c4ab559e3e2fe14272df2f9540b
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-08-01 19:12:40 +0530

    def is_prime(num):     if num < 2:         return False     for i in range(2, int(num ** 0.5) + 1):         if num % i == 0:             return False     return True start = int(input()) end = int(input()) count_primes = sum(1 for num in range(start, end + 1) if is_prime(num)) print(count_primes)

---
+++ Primes_in_range.py
@@ -0,0 +1,11 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+start = int(input())
+end = int(input())
+count_primes = sum(1 for num in range(start, end + 1) if is_prime(num))
+print(count_primes)
