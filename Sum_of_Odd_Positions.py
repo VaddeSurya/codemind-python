8dfbb6141400a53fe48219752d84682cab309e45
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-08-08 04:59:51 +0530

    n = int(input()) arr = list(map(int, input().split())) odd=0 for i in range(len(arr)):     if i % 2 != 0:         odd += arr[i] print(odd)

--- Sum_of_Odd_Positions.py
+++ Sum_of_Odd_Positions.py
@@ -1,7 +1,7 @@
 n = int(input())
 arr = list(map(int, input().split()))
-even = 0
+odd=0
 for i in range(len(arr)):
     if i % 2 != 0:
-        even += arr[i]
-print(even)
+        odd += arr[i]
+print(odd)
