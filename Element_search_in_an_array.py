1b7cf2f0ba1752e6da93901e9197ce4035ca2935
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-08-08 04:55:49 +0530

    n = int(input()) arr = list(map(int, input().split())) se = int(input()) found = False for i in arr:     if se == i:         found = True         break if found:     print("True") else:     print("False")

--- Element_search_in_an_array.py
+++ Element_search_in_an_array.py
@@ -1,5 +1,12 @@
-n=int(input())
-array = list(map(int, input().split()))
-find = int(input())
-result = find in array
-print(result)
+n = int(input())
+arr = list(map(int, input().split()))
+se = int(input())
+found = False
+for i in arr:
+    if se == i:
+        found = True
+        break
+if found:
+    print("True")
+else:
+    print("False")
