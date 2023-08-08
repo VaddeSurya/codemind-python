608912c58240c4b58d8db9e8c9b49c6ae0c6ac53
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-08-08 19:01:41 +0530

    n = int(input()) arr = list(map(int, input().split())) strictly_even = True for i, num in enumerate(arr):     if num % 2 != 0 and i % 2 == 0:         strictly_even = False         break
    if strictly_even:
        print("True")
    else:
        print("False")

---
+++ Strictly_ODD.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = list(map(int, input().split()))
+strictly_even = True
+for i, num in enumerate(arr):
+    if num % 2 != 0 and i % 2 == 0:
+        strictly_even = False
+        break
+
+if strictly_even:
+    print("True")
+else:
+    print("False")
