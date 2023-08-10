419201066f48f0f16f235a1057045d1810985d07
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-08-10 18:34:00 +0530

    n=int(input()) arr=list(map(int,input().split())) firsthalf=0 secondhalf=0; hfindex=len(arr)//2 for i in range(hfindex):     firsthalf+=arr[i] for i in range(hfindex,len(arr)):     secondhalf+=arr[i] print(secondhalf-firsthalf)

---
+++ difference_between_sum_of_first_half_and_second_half_in_an_array.py
@@ -0,0 +1,10 @@
+n=int(input())
+arr=list(map(int,input().split()))
+firsthalf=0
+secondhalf=0;
+hfindex=len(arr)//2
+for i in range(hfindex):
+    firsthalf+=arr[i]
+for i in range(hfindex,len(arr)):
+    secondhalf+=arr[i]
+print(secondhalf-firsthalf)
