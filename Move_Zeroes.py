6a3ac403b929ecbc7c32fde450700920a9c53d15
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-08-13 16:20:19 +0530

    n=int(input()) arr=list(map(int,input().split())) zeros=[] nonzeros=[] for i in arr:     if i==0:         zeros.append(i)     else:         nonzeros.append(i) res=zeros+nonzeros print(*res)

---
+++ Move_Zeroes.py
@@ -0,0 +1,11 @@
+n=int(input())
+arr=list(map(int,input().split()))
+zeros=[]
+nonzeros=[]
+for i in arr:
+    if i==0:
+        zeros.append(i)
+    else:
+        nonzeros.append(i)
+res=zeros+nonzeros
+print(*res)
